7fa88a254a92b278a27d07b7490664f1690f8352
christinasongue64-lgtm <christinasongue64@gmail.com>
2026-06-11 15:28:17 +0200

    fix render database config

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,18 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+
     list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'is_active', 'date_joined']
     list_filter = ['role', 'is_active', 'is_staff']
     search_fields = ['username', 'email', 'first_name', 'last_name']
     ordering = ['-date_joined']
+
     fieldsets = UserAdmin.fieldsets + (
         ('Informations ferme', {'fields': ('role', 'phone')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informations ferme', {
+            'fields': ('role', 'phone')
+        }),
     )
